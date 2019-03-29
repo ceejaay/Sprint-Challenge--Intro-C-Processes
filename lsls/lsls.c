6bea018e0bd476a0998f69963a3d1987cecbb951
Chad Jemmett <chad.jemmett@gmail.com>
2019-03-29 16:02:22 -0600

    Finished with the project

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,15 +6,15 @@
  * Main
  */
 
-char *concat(char *s1, char *s2)
-{
-  char *result = malloc(strlen(s1) + strlen(s2) + 1);
-  strcpy(result, s1);
-  strcat(result, "/");
-  /* printf(" inside concat: %s\n", result); */
-  strcat(result, s2);
-  return result;
-}
+/* char *concat(char *s1, char *s2) */
+/* { */
+/*   char *result = malloc(strlen(s1) + strlen(s2) + 1); */
+/*   strcpy(result, s1); */
+/*   strcat(result, "/"); */
+/*   /1* printf(" inside concat: %s\n", result); *1/ */
+/*   strcat(result, s2); */
+/*   return result; */
+/* } */
 
 int main(int argc, char **argv)
 {
@@ -22,55 +22,44 @@ int main(int argc, char **argv)
   DIR *d;
   struct dirent *dir;
   struct stat buf;
-  /* printf("arg 1 %s\n", argv[1]); */
-  /* d = opendir("."); */
-  /* int length = 0; */
-  /* while(argv[length] != '\0') */
-  /* { */
-  /*   printf("args: %s\n", argv[length]); */
-  /*   length++; */
-  /* } */
-  /* stat(".", &buf); */
-  /* printf("size of file my prog => %lld\n",  buf.st_size); */
-  /* printf("checking for argv"); */
-  if(argc >= 1)
+  char *address;
+
+  /* printf("Length of arg C %d\n", argc); */
+  if(argv[1])
   {
-    printf("Arg 0 => %s Arg 1 => %s\n", argv[0], argv[1]);
+    /* printf("Arg 0 => %s Arg 1 => %s\n", argv[0], argv[1]); */
     d = opendir(argv[1]);
+    address = strcat(argv[1], "/");
     /* printf("here"); */
-  } else
-
-  {
+  } else {
+    /* printf("Is is even geting here?\n"); */
     d = opendir(".");
-    printf("Arg 0 => %s Arg 1 => %s\n", argv[0], argv[1]);
-  }
+    address = "./";
+    /* printf("is this null? %d\n", d == NULL); */
+  };
 
 
-  /* dir = readdir(d); */
-  /* printf("directory name: %s\n", dir->d_name); */
-  /* printf("argv: %s\n", argv[1]); */
-  /* printf("D value => %d\n", d == '\0'); */
-
-  /* d = opendir("."); */
-  /* thing = readdir(d); */
-
-
-    /* printf("%s\n", thing->d_name); */
+  /* printf("concatted things => %s", strcat(argv[1], "/")); */
+    /* printf(" This is just before checking for null on d. \n If no Arg 1 => %s Arg 1 => %s\n", argv[0], argv[1]); */
+    /* printf("is this null before checking? %d\n", d == NULL); */
   if (d == NULL)
   {
     /* really we need an error here */
     fprintf(stderr, "No such directory\n");
     exit(1);
   } else {
-    printf("Starting while loop");
+    /* printf(" This is before while loop.  \n If no Arg 1 => %s Arg 1 => %s\n", argv[0], argv[1]); */
+    /* printf("is this null? %d\n", d == NULL); */
+    /* printf("Starting while loop"); */
+    /* printf("Address: %s\n", address); */
     while((dir = readdir(d)) != NULL)
     {
-      /* if arg 1 == . or .. skip it */
-      char *address = concat(argv[1], dir->d_name);
-      /* printf("full path %s\n", address); */
-      stat(address, &buf);
+      /* printf("path %s\n", address); */
+      char *new_address = strdup(address);
+      /* printf("path to file %s\n", strcat(new_address, dir->d_name)); */
+      stat(strcat(new_address, dir->d_name), &buf);
       printf("%10lld %s\n", buf.st_size, dir->d_name);
-      free(address);
+      /* free(address); */
     }
     closedir(d);
   }
